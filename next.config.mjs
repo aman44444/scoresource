846313c51ce07bc714f211b323512d839f90d073
aman1357 <amancorp04@gmail.com>
2025-09-15 14:54:28 +0530

    setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // next.config.js
+  
     images: {
         domains: ['cricketchampion.co.in','www.livescore.com'],
         domains: ['www.livescore.com'], // Add the domain for team images
       },
+      reactStrictMode: true,
 };
 
 export default nextConfig;
