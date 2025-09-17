2dcc9eb91bf1b7d86b2fd5c13fe0a7bc10ddd9d7
aman1357 <amancorp04@gmail.com>
2025-09-17 15:06:50 +0530

    added Accordion and skeleton

--- app/components/scores/Football/FootballScore.tsx
+++ app/components/scores/Football/FootballScore.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-
+import ScoreSkeleton from '../../common/ScoreSkeleton';
+import Accordion from '../../common/Accordion';
 
 interface Team {
   name: string;
@@ -18,12 +19,15 @@ interface Match {
   awayTeam: Team;
 }
 
+
+
 const FootballScore: React.FC = () => {
   const [matches, setMatches] = useState<Match[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
   const fetchLiveFootballMatches = async (): Promise<void> => {
+    console.log('click')
     const url = 'https://flashlive-sports.p.rapidapi.com/v1/events/live-list?locale=en_INT&sport_id=1&timezone=-4';
     const options = {
       method: 'GET',
@@ -34,82 +38,46 @@ const FootballScore: React.FC = () => {
     };
 
     setLoading(true);
-    setError(null);
+    setError(null)
 
-    try {
+      try {
       const response = await fetch(url, options);
-      const data = await response.json();
+      const result = await response.json();
 
-      if (data && Array.isArray(data.DATA)) {
-
-        const parsedMatches: Match[] = data.DATA.flatMap((tournament: {
-          EVENTS: Array<{
-            EVENT_ID: string;
-            START_TIME: number;
-            STAGE: string;
-            GAME_TIME: string;
-            ROUND: string;
-            HOME_NAME: string;
-            SHORTNAME_HOME: string;
-            HOME_IMAGES: string[];
-            HOME_SCORE_CURRENT: string;
-            AWAY_NAME: string;
-            SHORTNAME_AWAY: string;
-            AWAY_IMAGES: string[];
-            AWAY_SCORE_CURRENT: string;
-          }>
-        }) => 
-          (tournament.EVENTS || []).map((event) => ({
-            eventId: event.EVENT_ID,
-            startTime: event.START_TIME,
-            stage: event.STAGE,
-            gameTime: event.GAME_TIME,
-            round: event.ROUND,
-            homeTeam: {
-              name: event.HOME_NAME,
-              shortName: event.SHORTNAME_HOME,
-              logoUrl: event.HOME_IMAGES[0], 
-              score: event.HOME_SCORE_CURRENT,
-            },
-            awayTeam: {
-              name: event.AWAY_NAME,
-              shortName: event.SHORTNAME_AWAY,
-              logoUrl: event.AWAY_IMAGES[0], 
-              score: event.AWAY_SCORE_CURRENT,
-            }
-          }))
-        );
-
-        setMatches(parsedMatches);
+      if (result.status) {
+        setMatches(result.data);
       } else {
-        setError('No live matches found.');
+        setError("No matches available");
       }
     } catch (err) {
-      setError('Error fetching matches: ' + (err as Error).message);
+      setError("Error fetching football matches");
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <div className='w-5/6'>
-       <button onClick={fetchLiveFootballMatches} className="text-sm sm:text-md  p-4 mb-4 w-full text-gray-500 border-b border-gray-500 sticky bg-black top-0 ">
-        Football Score
-      </button>
-
-      {loading && <p className='text-xs text-gray-300'>Loading...</p>}
-      {error && <p className='text-xs text-gray-300'>{error}</p>}
-     <div className='flex flex-col justify-center items-center '>
-      {matches.length > 0 && matches.map((match) => (
-        <div key={match.eventId} className=" w-full border p-4 mb-4 rounded-xl border-gray-500" >
     
+   <Accordion title='Football Score' onFetch={fetchLiveFootballMatches}>
+      {/* {loading && <p className='text-xs text-gray-300'>Loading...</p>} */}
+      {error && <p className='text-xs text-gray-300'>{error}</p>}
+       {loading &&
+          Array.from({ length: 4 }).map((_, i) => <ScoreSkeleton key={i} />)}
+      {!loading &&
+          matches.length > 0 && matches.map((match) => (
+        <div key={match.eventId} className=" w-full border p-4 mb-4 rounded-xl border-gray-500" >
+           
            <p className='text-xs'>Game Time: {match.gameTime}</p>
-          <div className="flex items-center mb-2">
+          <div className="flex items-center mb-2 justify-between">
+            <div>
             <img src={match.homeTeam.logoUrl} alt={match.homeTeam.name} className="w-4 h-4 mr-2" />
             <span className='text-xs'>{match.homeTeam.shortName}</span>
+            </div>
             <span className=" text-xs mx-2">vs</span>
-            <img src={match.awayTeam.logoUrl} alt={match.awayTeam.name} className="w-4 h-4 mr-2" />
-            <span className='text-xs'>{match.awayTeam.shortName}</span>
+           <div>
+           <img src={match.awayTeam.logoUrl} alt={match.awayTeam.name} className="w-4 h-4 mr-2" />
+           <span className='text-xs'>{match.awayTeam.shortName}</span>
+           </div>
           </div>
           <div className='w-full flex justify-center '>
            
@@ -119,8 +87,8 @@ const FootballScore: React.FC = () => {
           <p className='text-xs'>Stage: {match.stage}</p>
         </div>
       ))}
-      </div>
-    </div>
+      
+   </Accordion>
   );
 };
 
