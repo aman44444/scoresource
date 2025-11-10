29e2836a0cc76dda71d9cc2ce300a4c6efb21fd8
aman1357 <amancorp04@gmail.com>
2025-11-10 16:44:59 +0530

    added client component wrapper

---
+++ app/components/FeedClient.tsx
@@ -0,0 +1,28 @@
+"use client"
+import { useState } from "react";
+import NewsFeed from "./NewsFeed";
+
+const Sports = ["Football", "Cricket" , "Tennis"];
+
+const FeedClient = () => {
+    const [selectedTopic, setSelectedTopic] = useState("Football")
+    return (
+        <>
+          <div className="bg-black h-full border no-scrollbar overflow-y-auto border-gray-600 w-full sm:w-6/12 rounded-xl">
+            <div className="flex items-center justify-around w-full backdrop-blur-lg bg-transparent h-10 sm:h-16 sticky top-0 text-gray-500 border-b border-gray-500">
+             {Sports.map((topic) => (
+                <button 
+                key={topic}
+                onClick={() => setSelectedTopic(topic)}>
+                     {topic}
+                </button>
+             ))}
+
+          </div>
+            <NewsFeed selectedTopic={selectedTopic}/>
+        </div>
+        </>
+    )
+}
+
+export default FeedClient;
