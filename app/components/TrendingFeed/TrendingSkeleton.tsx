f0f08b90321fec65f081478c14e3ac7cd3e8ab21
aman1357 <amancorp04@gmail.com>
2025-11-20 12:57:39 +0530

    feat(trending) : add skeleton loaders for trending sections

---
+++ app/components/TrendingFeed/TrendingSkeleton.tsx
@@ -0,0 +1,14 @@
+const TrendingSkeleton = () => {
+  return (
+    <ul className="space-y-2">
+      {[1, 2, 3].map(i => (
+        <li key={i} className="flex justify-between items-center">
+          <div className="h-3 w-24 bg-gray-700/40 rounded animate-pulse" />
+          <div className="h-3 w-3 bg-gray-700/40 rounded-full animate-pulse" />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TrendingSkeleton;
