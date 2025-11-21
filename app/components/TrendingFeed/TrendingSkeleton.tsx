480cb29802c137a8117ddce52e97f538cab00aab
aman1357 <amancorp04@gmail.com>
2025-11-21 15:16:54 +0530

    style(trendingPlayer): add blur on tooltip

--- app/components/TrendingFeed/TrendingSkeleton.tsx
+++ app/components/TrendingFeed/TrendingSkeleton.tsx
@@ -3,8 +3,8 @@ const TrendingSkeleton = () => {
     <ul className="space-y-2 mr-4 ml-4">
       {[1, 2, 3].map(i => (
         <li key={i} className="flex justify-between items-center">
-          <div className="h-3 w-24 bg-gray-700/40 rounded animate-pulse" />
-          <div className="h-3 w-3 bg-gray-700/40 rounded-full animate-pulse" />
+          <div className="h-3 w-24 bg-gray-700/50 rounded animate-pulse" />
+          <div className="h-3 w-3 bg-gray-700/50 rounded-full animate-pulse" />
         </li>
       ))}
     </ul>
