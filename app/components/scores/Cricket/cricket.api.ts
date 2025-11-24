6150a365a51f8a811f5ca8b06c4167623cc66b41
aman1357 <amancorp04@gmail.com>
2025-11-24 12:25:48 +0530

    refactor(CricketScore): add cricketscore api

---
+++ app/components/scores/Cricket/cricket.api.ts
@@ -0,0 +1,20 @@
+export const fetchCricketMatches = async () => {
+    const result = await fetch(
+         'https://cricket-live-line1.p.rapidapi.com/liveMatches',
+    
+         {
+            headers: {
+                 'x-rapidapi-key': process.env.NEXT_PUBLIC_API_KEY || '',
+                 'x-rapidapi-host': 'cricket-live-line1.p.rapidapi.com',
+            },
+         }
+    );
+
+    const json = await result.json();
+
+    if (!json.status) {
+        throw new Error("No cricket matches");
+    }
+
+    return json.data;
+}
