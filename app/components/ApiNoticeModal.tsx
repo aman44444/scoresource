7b37c57d169519c88d71748e92634c3a3b4a8be9
aman1357 <amancorp04@gmail.com>
2026-08-18 23:23:37 +0530

    style: redesign modal button

--- app/components/ApiNoticeModal.tsx
+++ app/components/ApiNoticeModal.tsx
@@ -28,13 +28,11 @@ export default function ApiNoticeModal() {
         </h2>
 
         <p className="mt-4 text-gray-300 leading-7">
-          ScoreSource relies on third-party sports and news APIs to deliver
-          live scores, fixtures, standings, and news.
+          ScoreSource relies on third-party sports and news APIs to deliver live
+          scores, fixtures, standings, and news.
         </p>
 
-        <p className="mt-4 text-gray-300">
-          You may occasionally experience:
-        </p>
+        <p className="mt-4 text-gray-300">You may occasionally experience:</p>
 
         <ul className="mt-3 space-y-2 list-disc list-inside text-gray-400">
           <li>Missing live scores</li>
@@ -45,12 +43,46 @@ export default function ApiNoticeModal() {
         </ul>
 
         <p>
-           ScoreSource relies on trusted third-party providers to deliver today&apos;s live scores.
+          ScoreSource relies on trusted third-party providers to deliver
+          today&apos;s live scores.
         </p>
-   
+
         <button
           onClick={closeModal}
-          className="mt-6 w-full rounded-lg bg-white py-3 font-semibold text-black transition hover:bg-gray-200"
+          className="
+             mt-6
+            w-full
+            py-2.5
+            rounded-xl
+ 
+             font-medium
+           text-white
+
+            bg-black/10
+          hover:bg-black/50
+
+            border
+          border-gray-500/80
+
+           backdrop-blur-none
+
+    
+
+         hover:scale-[0.98]
+
+         hover:shadow-[inset_0_1px_2px_rgba(255,255,255,0.45),
+                   inset_0_-2px_3px_rgba(0,0,0,0.5),
+                   0_3px_0_rgba(255,255,255,0.08),
+                   0_7px_0_rgba(0,0,0,0.4),
+                   0_14px_25px_rgba(0,0,0,0.4)]
+
+               active:translate-y-[2px]
+           active:scale-[0.97]
+
+            transition-all
+             duration-200
+          shadow-[9px_16px_20px_26px_rgba(0,_0,_0,_0.1)]
+          "
         >
           Continue
         </button>
