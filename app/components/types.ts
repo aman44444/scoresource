47de7264b60093d361b44c2374fb28b87d28f105
aman1357 <amancorp04@gmail.com>
2026-08-21 23:39:23 +0530

    feat: add match and team types

--- app/components/types.ts
+++ app/components/types.ts
@@ -9,3 +9,20 @@ export interface TrendingPlayer {
   name: string;
   reason: string;
 }
+
+export interface Team {
+  name: string;
+  shortName: string;
+  logoUrl: string;
+  score: string;
+}
+
+export interface Match {
+  eventId: string;
+  startTime: number;
+  stage: string;
+  gameTime: string;
+  round: string;
+  homeTeam: Team;
+  awayTeam: Team;
+}
